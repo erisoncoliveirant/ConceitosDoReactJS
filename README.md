ca91685ec48d23e116cea8030125b003e37401ba
Erison <erisoncoliveirant@gmail.com>
2020-06-16 20:56:53 -0400

    Entrega do desafio

---
+++ README.md
@@ -0,0 +1,34 @@
+# Conceitos-Node.js
+Desafio: Conceitos do ReactJS, Bootcamp GoStack 12 Rocketseat.
+
+## Sobre
+Conceitos do ReactJS metas de aprendizagem.
+
+## Requisitos
+- [x] Node.js
+- [x] npm
+- [x] yarn
+- [x] axios
+- [x] react
+- [x] react-dom
+- [x] VSCode
+- [x] Git
+
+
+## Instalação
+### 1º Passo
+```git clone https://github.com/eoliveira90/ConceitosDoReactJS.git```
+
+### 2º Passo - instalação de dependencias
+```yarn install```
+
+### 3º Passo - roda o projeto
+```npm run start```
+
+### Passo OPCIONAL
+```npm run test```
+```npm run build```
+```npm run eject```
+
+## Autor
+Erison Correia de Oliveira Neto - [GitHub](https://github.com/eoliveira90) / [Email](mailto:erisoncoliveirant@gmail.com)
--- src/App.js
+++ src/App.js
@@ -1,31 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(repositories => {
+      setRepositories(repositories.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post('repositories', {
+      title: "Desafio ReactJS",
+      url: "https://github.com/eoliveira90",
+      techs: ['HTML5', 'CSS3', 'Node JS', 'TypeScript']
+    });
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const filteredRepositories = repositories.filter( (repository) => repository.id !== id );
+    setRepositories(filteredRepositories);
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+    <>
+      <div>
+        <ul data-testid="repository-list">
+          {repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
           </button>
-        </li>
-      </ul>
+            </li>
+          ))}
+        </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    </>
   );
 }
+/*
+<div className="container" style={{ 'display': 'none' }}>
+        <div className="row">
+          <div className="col-md-5">
 
+            <h4 className="text-center font-weight-bold">Post creation form</h4>
+            <div className="form-group">
+              <input type="text" placeholder="Post title" v-model="post.title" className="form-control" />
+            </div>
+            <div className="form-group">
+              <textarea v-model="post.content" placeholder="Post content" className="form-control"></textarea>
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block btn-primary" onClick={handleAddRepository}>Adicionar</button>
+            </div>
+          </div>
+          <div className="col-md-7">
+
+
+            <h4 className="text-center font-weight-bold">Posts</h4>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Techs</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="post in posts">
+                  <td>title</td>
+                  <td>techs</td>
+                  <td>
+                    <button className="btn btn-danger" onClick={() => handleRemoveRepository(1)}>Remover</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+      </div>
+*/
 export default App;
